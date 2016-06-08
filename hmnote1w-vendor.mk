b4ab02f2b09848215e3a5a0ee22c262cf93e122a
Rohan Taneja <rohantaneja63@gmail.com>
2016-06-08 15:21:30 +0530

    hmnote1w: add vendor blobs

---
+++ hmnote1w-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hmnote1w/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hmnote1w/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hmnote1w/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hmnote1w/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hmnote1w/proprietary/vendor,system/vendor)
