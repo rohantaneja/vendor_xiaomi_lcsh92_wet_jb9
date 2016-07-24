eb165a45a4ae50a2ac7a4b2f19499dd178a9aa6d
Rohan Taneja <rohantaneja63@gmail.com>
2016-07-24 18:53:34 +0530

    use codename: hmnote1w -> lcsh92_wet_jb9

---
+++ lcsh92_wet_jb9-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/lcsh92_wet_jb9/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/lcsh92_wet_jb9/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/lcsh92_wet_jb9/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/lcsh92_wet_jb9/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/lcsh92_wet_jb9/proprietary/vendor,system/vendor)
